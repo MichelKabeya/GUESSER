719bd8a787aa9f072b98d93f4b50b79427502bd8
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-05-29 09:46:26 +0200

    added the slection controls

--- app/src/main/java/com/example/guessnumber/MainActivity.kt
+++ app/src/main/java/com/example/guessnumber/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.guessnumber
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // random number between 1 and 100
+    private val randomNumber = (1..100).random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        //VARIABLE DECLARATIONS
+        val guess = findViewById<EditText>(R.id.userInput)
+        val guessBtn = findViewById<Button>(R.id.guessBtn)
+        val textResult = findViewById<TextView>(R.id.textResult)
+
+        //BUTTON CLICK LISTENER
+        guessBtn.setOnClickListener {
+            val userGuess = guess.text.toString().toInt()
+            if (userGuess == randomNumber) {
+                textResult.text = "Congratulations! You guessed the number."
+            } else if (userGuess < randomNumber) {
+                textResult.text = "The number is higher."
+            } else {
+                textResult.text = "The number is lower."
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
